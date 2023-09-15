1cd5360067371a269ed99262259539afe65d878a
ashikurbd71 <ashikurovi2003@gmail.com>
2023-09-15 15:15:03 +0600

    responsive porblem fixed

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Heading = () => {
     return (
         <div>
-            <h1 className=' text-4xl font-bold my-10 text-center'>Course Registration</h1>
+            <h1 className=' lg:text-4xl text-3xl font-bold my-10 text-center '>Course Registration</h1>
         </div>
     );
 };
--- src/Components/ShowInfos/Showinfos.jsx
+++ src/Components/ShowInfos/Showinfos.jsx
@@ -3,10 +3,10 @@ import Showinfo from '../Showinfo/Showinfo';
 
 const Showinfos = ({courses,TotalCredit,MinusCredit,Totalprice}) => {
     return (
-        <div className='w-[350px] h-full mb-10 rounded-lg mx-auto lg:flex-1 bg-[#FFFFFF]'>
+        <div className='lg:w-[350px] w-full h-full mb-10 rounded-lg mx-auto lg:flex-1 bg-[#FFFFFF]'>
 
             <h1 className='text-[#2F80ED] font-bold text-[16px] ml-5 my-4'>Credit Hour Remaining {MinusCredit} hr</h1> 
-<hr className=' mx-auto w-60 border-1'  />
+<hr className=' mx-auto lg:w-60 w-80 border-1'  />
 
 
          <h1 className='text-[#1C1B1B] font-bold text-[20px] ml-5 my-4'>Course Name</h1> 
@@ -14,10 +14,10 @@ const Showinfos = ({courses,TotalCredit,MinusCredit,Totalprice}) => {
             {
                 courses.map((course,idx) => <Showinfo course={course} key={course.id} idx={idx}></Showinfo>)
             }
-            <hr className=' mx-auto w-60 border-1 my-5' />
+            <hr className=' mx-auto lg:w-60 w-80 border-1 my-5' />
 
             <h1 className='text-[#1C1B1B]  font-bold text-[16px] ml-5 my-4'>Total Credit Hour : {TotalCredit}</h1>
-            <hr className=' mx-auto w-60 border-1 my-5' />
+            <hr className=' mx-auto lg:w-60 w-80 border-1 my-5' />
             <h1 className='text-[#1C1B1B] font-bold text-[16px] ml-5 my-4'>Total Price : {Totalprice} USD</h1>
         </div>
     );
